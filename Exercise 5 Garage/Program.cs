325bc937e29cb7e3a27a469c16a937a8195afc0c
CamillaRidstrom <camillaeridstrom@outlook.com>
2023-10-11 09:54:39 +0200

    Added summarising comment in Program.cs

--- Exercise 5 Garage/Program.cs
+++ Exercise 5 Garage/Program.cs
@@ -2,6 +2,18 @@
 
 public class Program
 {
+    /*  KOMMENTAR:
+
+    Programmet har alltså all funktionalitet som efterfrågades 
+    samt Unit Test (två test som testar en av (de två) publika funktionerna i Garage).
+
+    Men inte alla subklasser även om alla typerna av fordon finns.
+    Jag tror (?) programmet följer strukturritningen vi fick, men utan interfaceversioner. 
+
+    (Det finns ytterligare kommentarer i Handler, Garage och Vehicle).
+
+    */
+
     public static void Main(string[] args)
     {
         Manager manager = new Manager();
